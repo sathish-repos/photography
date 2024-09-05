db690e71ba9c5e6a7b703a6c169ca2da69b6a98e
sathish <me.sathish.git@gmail.com>
2024-09-05 22:59:05 +0530

    create gallery library

---
+++ projects/gallery/src/lib/gallery.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-gallery',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      gallery works!
+    </p>
+  `,
+  styles: ``
+})
+export class GalleryComponent {
+
+}
