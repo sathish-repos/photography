342e52c7acfec2babb74fbb1c168c5d337dffcdc
sathish <me.sathish.git@gmail.com>
2024-09-05 23:38:41 +0530

    setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GalleryComponent } from '../../projects/gallery/src/public-api';
+import { HomeComponent } from '../../projects/home/src/public-api';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'gallery', component: GalleryComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '' },
+];
