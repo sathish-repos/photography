581362cfd6dea3e6503c90dcdf774654c7448d5a
sathish <me.sathish.git@gmail.com>
2024-09-07 18:09:14 +0530

    create home template

---
+++ projects/home/src/lib/feature/home-template/home-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-home-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-template.component.html',
+  styleUrl: './home-template.component.scss'
+})
+export class HomeTemplateComponent {
+
+}
