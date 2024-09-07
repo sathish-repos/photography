76818e950d666922ee27662e8a0d2644ebc05fe6
sathish <me.sathish.git@gmail.com>
2024-09-07 19:39:41 +0530

    inject it to hero-template

--- projects/home/src/lib/feature/home-template/home-template.component.ts
+++ projects/home/src/lib/feature/home-template/home-template.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { Home } from '../../page/home/models/home.types';
 
 @Component({
   selector: 'lib-home-template',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './home-template.component.html',
   styleUrl: './home-template.component.scss'
 })
 export class HomeTemplateComponent {
-
+  content = input.required<Home>();
 }
